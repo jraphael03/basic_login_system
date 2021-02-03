6b320aa47b6054608860113a4bbb57ea347fce99
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-02 20:23:08 -0500

    Included bcrypt now when we create a new user the password will not be directly inserted into the db the hash will be

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const port = 5000;
@@ -22,13 +24,21 @@ app.post('/register', (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("INSERT INTO users (username, password) VALUES (?,?)", 
-    [username, password], (err, result) => {
-        if(err){
+    bcrypt.hash(password, saltRounds, (err, hash) => {              // USING BCRYPT AND HASH TO PROTECT THE PASSWORD
+        if (err) {
             console.log(err)
-        } else{
-            console.log('success');
         }
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hash],       //PASS HASH INSTEAD OF PASSWORD TO PASS THE HASH VERSION OF THE VARIABLE
+            (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("success");
+            }
+            }
+        );
     })
 })
 
@@ -39,15 +49,22 @@ app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, result) => {
+    db.query("SELECT * FROM users WHERE username = ?;", 
+        username, (err, result) => {
         if(err){
             res.send({err: err})
         } 
         //IF THERE ISN'T AN ERROR
             if(result.length > 0) {
-                res.send(result)
+                bcrypt.compare(password, result[0].password, (error, response) => {     // COMPARE TO THE RESULT OF THE PASSWORD TO THE USER WE ARE TRYING TO ACCESS
+                    if (response){
+                        res.send(result)            //if combination is correct
+                    } else{
+                        res.send({message: "Username and/or Password is incorrect"})       //if combination is incorrect
+                    }
+                })
             } else {
-                res.send({message: "Username and/or Password is incorrect"})
+                res.send({message: "User does not exist"})          //if user does not exist
             }
         }
     );
